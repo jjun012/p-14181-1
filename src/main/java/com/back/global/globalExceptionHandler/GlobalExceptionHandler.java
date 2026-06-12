f0b717fca49dd9ad4caaab9b4461337d5b5a10fa
jjun012 <duwns2057@naver.com>
2026-06-12 14:10:52 +0900

    009-2

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.back.global.globalExceptionHandler;
 
 import com.back.global.rsData.RsData;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
@@ -28,6 +30,31 @@ public class GlobalExceptionHandler {
                 NOT_FOUND
         );
     }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<RsData<Void>> handle(ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations()
+                .stream()
+                .map(violation -> {
+                    String field = violation.getPropertyPath().toString().split("\\.", 2)[1];
+                    String[] messageTemplateBits = violation.getMessageTemplate()
+                            .split("\\.");
+                    String code = messageTemplateBits[messageTemplateBits.length - 2];
+                    String _message = violation.getMessage();
+
+                    return "%s-%s-%s".formatted(field, code, _message);
+                })
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
+
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "400-1",
+                        message
+                ),
+                BAD_REQUEST
+        );
+    }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<RsData<Void>> handle(MethodArgumentNotValidException ex) {
