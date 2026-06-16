2ba30416323889de79a729fdf1df038ba7ebac12
jjun012 <duwns2057@naver.com>
2026-06-16 15:36:48 +0900

    refactor: getActor 메서드 내의 로직 중 일부를 Rq::getHeader 함수로 옮김

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -21,9 +21,9 @@ public class Rq {
 
     public Member getActor() {
         String apiKey;
-        String headerAuthorization = req.getHeader("Authorization");
+        String headerAuthorization = getHeader("Authorization", "");
 
-        if (headerAuthorization != null && !headerAuthorization.isBlank()) {
+        if (!headerAuthorization.isBlank()) {
             if (!headerAuthorization.startsWith("Bearer "))
                 throw new ServiceException("401-2", "Authorization 헤더가 Bearer 형식이 아닙니다.");
 
@@ -40,6 +40,14 @@ public class Rq {
 
         return member;
     }
+    
+    private String getHeader(String name, String defaultValue) {
+        return Optional
+                .ofNullable(req.getHeader(name))
+                .filter(headerValue -> !headerValue.isBlank())
+                .orElse(defaultValue);
+    }
+
     private String getCookieValue(String name, String defaultValue) {
         return Optional
                 .ofNullable(req.getCookies())
