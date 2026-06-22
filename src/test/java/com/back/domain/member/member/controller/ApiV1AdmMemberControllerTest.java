db28090ccb6c7e78fc7a73654cc0b21098203126
jjun012 <duwns2057@naver.com>
2026-06-22 15:46:43 +0900

    feat: 관리자가 아니라면 관지라용 액션 메서드에 집입 불가

--- src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
@@ -74,8 +74,6 @@ public class ApiV1AdmMemberControllerTest {
                 .andDo(print());
 
         resultActions
-                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
-                .andExpect(handler().methodName("getItems"))
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.resultCode").value("403-1"))
                 .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
@@ -124,8 +122,6 @@ public class ApiV1AdmMemberControllerTest {
                 .andDo(print());
 
         resultActions
-                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
-                .andExpect(handler().methodName("getItem"))
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.resultCode").value("403-1"))
                 .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
