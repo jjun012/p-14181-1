b7497c37ff20e0edca0f35053b52ff38df5bff39
jjun012 <duwns2057@naver.com>
2026-06-23 10:08:47 +0900

    feat: 시큐리티 예외처리에서 잭슨 사용

--- src/main/java/com/back/standard/util/Ut.java
+++ src/main/java/com/back/standard/util/Ut.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ClaimsBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import tools.jackson.databind.ObjectMapper;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -51,6 +52,7 @@ public class Ut {
 
             return true;
         }
+
         public static Map<String, Object> payload(String secret, String jwtStr) {
             SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes());
 
@@ -64,6 +66,22 @@ public class Ut {
                 );
             } catch (Exception e) {
                 return null;
+            }
+        }
+    }
+
+    public static class json {
+        private static final ObjectMapper objectMapper = new ObjectMapper();
+
+        public static String toString(Object object) {
+            return toString(object, null);
+        }
+
+        public static String toString(Object object, String defaultValue) {
+            try {
+                return objectMapper.writeValueAsString(object);
+            } catch (Exception e) {
+                return defaultValue;
             }
         }
     }
